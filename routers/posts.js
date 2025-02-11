378d775d858dcd6cbf2eef98ff5aebe8b5e4cae1
fabiodagostino27 <fabiodago27@gmail.com>
2025-02-11 12:03:51 +0100

    ADD: show

--- routers/posts.js
+++ routers/posts.js
@@ -6,4 +6,9 @@ router.get("/", (req, res) => {
    res.send("Lista dei post") 
 });
 
+// Show
+router.get("/:id", (req, res) => {
+    res.send(`Mostra post ${req.params.id}`)
+});
+
 module.exports = router
